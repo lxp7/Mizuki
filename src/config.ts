56e70977d3e25a509b3bc356532aafdb7d1c798e
凉小皮 <852061305@qq.com>
2026-03-03 10:09:42 +0800

    自动部署测试

--- src/config.ts
+++ src/config.ts
@@ -155,7 +155,7 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true, // 在主页显示自定义文本
-			title: "わたしの部屋", // 主页横幅主标题
+			title: "凉皮好吃", // 主页横幅主标题
 
 			subtitle: [
 				"特別なことはないけど、君がいると十分です",
